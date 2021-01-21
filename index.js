db42fe6f7691a8809cd9f235248e77bd9d792da8
lambor590 <lambor590@gmail.com>
2021-01-21 16:32:29 +0100

    noice

--- index.js
+++ index.js
@@ -14,21 +14,6 @@ const ElizaHelper = require('./eliza');
 const LiveEmbed = require('./live-embed');
 const MiniDb = require('./minidb');
 
-const keepAlive = require('./server');
-const Monitor = require('ping-monitor');
- 
-keepAlive();
-const monitor = new Monitor({
-  website: 'https://KeepAliveGlobalTWITCH.lambor590.repl.co',
-  title: 'Secundario',
-  interval: 30 // minutes
-});
- 
-monitor.on('up', (res) => console.log(`${res.website} está encedido.`));
-monitor.on('down', (res) => console.log(`${res.website} se ha caído - ${res.statusMessage}`));
-monitor.on('stop', (website) => console.log(`${website} se ha parado.`) );
-monitor.on('error', (error) => console.log(error));
-
 // --- Startup ---------------------------------------------------------------------------------------------------------
 console.log('Iniciando...');
 
